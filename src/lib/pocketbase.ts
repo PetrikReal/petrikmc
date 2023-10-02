d018c78ec0092256f37ff0d9397df5ae2a19aa6a
Berényi Bence <69584313+drinkmonster@users.noreply.github.com>
2023-10-02 16:27:09 +0200

    Update pocketbase.ts

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('https://late-architect.pockethost.io/');
+export const pb = new PocketBase('https://pb.petrikreal.tech/');
 
 export const currentUser = writable(pb.authStore.model);
 
