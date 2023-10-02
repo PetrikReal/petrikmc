5fd3a0bfaddbc28d47af431cd6c737f07f309f4c
drinkmonster <berenyibence98@gmail.com>
2023-10-02 10:47:31 +0200

    balls

--- src/lib/componenets/+NavBar.svelte
+++ src/lib/componenets/+NavBar.svelte
@@ -6,18 +6,17 @@
       <div class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
-            <img src="%sveltekit.assets%/profile.png" />
+            <img src="profile.png" />
           </div>
         </label>
         <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
           <li>
             <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
+              Profil
             </a>
           </li>
-          <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
+          <li><a>Beállítások</a></li>
+          <li><a>Kijelentkezés</a></li>
         </ul>
       </div>
     </div>
