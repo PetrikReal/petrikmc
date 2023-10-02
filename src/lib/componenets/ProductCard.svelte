76917ce0c8d152b263a9b8487ff57fed6352d94d
drinkmonster <berenyibence98@gmail.com>
2023-10-02 17:16:43 +0200

    nincs ehhez kedvem

---
+++ src/lib/componenets/ProductCard.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<div class="h-96 w-48 rounded-lg bg-stone-800">
+    <h1 class="font-black text-center text-2xl">Balls</h1>
+</div>
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -2,6 +2,9 @@
   import Navbar from '$lib/componenets/Navbar.svelte';
   import { currentUser } from '$lib/pocketbase';
   import LoginView from '$lib/views/LoginView.svelte';
+  import ProductCard from '$lib/componenets/ProductCard.svelte';
+
+  
 </script>
 
 {#if $currentUser}
@@ -9,6 +12,7 @@
     <Navbar />
     <main class="h-full m-2 rounded-xl shadow-2xl p-2 bg-stone-900 box-border">
       <h1 class="text-center font-black p-4 text-4xl">Bolt</h1>
+      <ProductCard />
     </main>
   </div>
 {:else}
